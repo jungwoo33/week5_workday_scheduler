c0355ada749193c603b28bae6b53e36ede58bec3
Jungwoo Lee <jungwoo33@gmail.com>
2022-11-16 22:19:11 -0600

    v2 updated

--- old_codes/script_v2.js
+++ old_codes/script_v2.js
@@ -57,8 +57,66 @@ for(var i=0;i<9;i++){
 }
 */
 
+var my_schedule = []; // create empty schedule at the beginning
+$("button").click(function(){
+    //console.log($(this).parent()[0]); // <div id="hour-9" ~
+    //console.log($(this).parent()[0].children[1]); // <textarea class = ~
+    //console.log($(this).parent()[0].children[1].value); // this returns the input value
+
+    // so, now let's start
+    var input_text = $(this).parent()[0].children[1].value
+    var div_id = $(this).parent().attr('id'); // hour-9
+    var id_hr2 = div_id.split("-"); // ['hour','9']
+    var id_hr = id_hr2[1]; // 9
+    for(var i=0;i<9;i++){
+        var schedule_hr = i+9;
+        if(schedule_hr === Number(id_hr)){
+            my_schedule[i] = input_text;
+            //console.log(i);
+            //console.log(my_schedule[i]);
+            console.log(input_text);
+
+            /* Note: here, the original "setItem" approach does not work correctly.
+                it also store an empty space and comma as an array value.
+                So, it is difficult to handle the data.
+                That is why I use JSON approach.
+            */
+            //localStorage.setItem("my_schedule",my_schedule); // store in the localStorage
+            localStorage["my_schedule"]= JSON.stringify(my_schedule); // store in the localStorage
+        }
+    }
+});
+
+function save_data(){
+    //var save_button = 
+}
+function retrieve_data(){
+    //my_schedule = localStorage.getItem("my_schedule"); // save stored data to the global variable
+    //console.log(my_schedule);
+    my_schedule = JSON.parse(localStorage["my_schedule"]); // save stored data to the global variable
+    
+    // then, clear the saved localstorage value:
+    // Note, it will be generated and saved again when a new schedule is saved.
+    localStorage.removeItem("my_schedule");
+
+    // display the stored schedule
+    for(var i=0;i<9;i++){
+        if(my_schedule[i]){
+            // if it is not empty, retrieve the data
+            $("textarea")[i].value = my_schedule[i];
+        }
+    }
+}
+retrieve_data();
+
+//var jw = $("textarea")[0].value = 'jw';
+//console.log($("textarea")[0].value);
 
 
+
+/* Note: I have to concern AM/PM also.
+    current version, does not distinguish 10am and 10pm
+*/
 function update_past_present_future(){
     var today = new Date();
     hour = today.getHours(); // 'number'
@@ -86,72 +144,11 @@ update_past_present_future();
 
 
 
-// let's try with a basic approach
-/*
-var btn9  = $('#hour-9'); // this is for test
-var btn10 = $('#hour-10'); // this is for test
-var btn11 = $('#hour-11'); // this is for test
-var btn12 = $('#hour-12'); // this is for test
-var btn13 = $('#hour-13'); // this is for test
-var btn14 = $('#hour-14'); // this is for test
-var btn15 = $('#hour-15'); // this is for test
-var btn16 = $('#hour-16'); // this is for test
-var btn17 = $('#hour-17'); // this is for test
-btn9.on('click', save_schedule);
-btn10.on('click', save_schedule);
-btn11.on('click', save_schedule);
-btn12.on('click', save_schedule);
-btn13.on('click', save_schedule);
-btn14.on('click', save_schedule);
-btn15.on('click', save_schedule);
-btn16.on('click', save_schedule);
-btn17.on('click', save_schedule);
-function save_schedule(){
-  console.log('jw');
-}
-*/
-
-
-
-
-
-
 function display_today(){
   var today = new Date();
-
-  // let's try several functions first....
-  console.log(today); // Wed Nov 16 2022 09:17:27 GMT-0600 (Central Standard Time)
-  console.log(typeof today); // object. So, I cannot directly split this into string
-  console.log(today.getDay); // n'th day of the week (0 ~ 6). So, this is not what I am looking for.
-  console.log(today.toDateString()); // Wed Nov 16 2022; this is what I am looking for
-  console.log(typeof today.toDateString()); // string
-  console.log(today.toLocaleDateString()); // 11/16/2022;
-  console.log(typeof today.toLocaleDateString()); // string
-  console.log(today.toTimeString()); // 09:39:15 GMT-0600 (Central Standard Time)
-  console.log(typeof today.toTimeString()); // string
-  console.log(today.toLocaleTimeString()); //09:40:36 AM
-  console.log(today.toString()); // Wed Nov 16 2022 09:17:27 GMT-0600 (Central Standard Time)
-  console.log(typeof today.toString()); // string
-  console.log(today.getHours()); // returs 0 ~ 23 in "number"
-  console.log(typeof today.getHours()); // number
-
-  // Now, I understand what I need to do^^
-  
-  var date_string = today.toDateString().split(" "); // Wed Nov 16 2022
-  var weekday = date_string[0]; //Wed
-  var month = date_string[1]; // Nov
-  var day = date_string[2]; // 16
-  var year = date_string[3]; // 2022
-  console.log(weekday, month, day, year);
-  
   var currentDay = $('#currentDay');
   //currentDay.text(weekday + ', ' + month + ', ' + day + ', ' + year);
   currentDay.text(today.toDateString()); // direct & simple approach  
 }
-
-//display_today();
-
-
-
-
+display_today();
 
